86ce1dcfd8fd1ae8930c179e4107f428ba381530
Valerian Roustit <valerian.roustit@gmail.com>
2020-06-16 14:08:28 +0100

    add Bus, Person, BusTest

---
+++ src/test/java/BusTest.java
@@ -0,0 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BusTest {
+    private Bus bus;
+    private Person person;
+
+    @Before
+    public void before(){
+        bus = new Bus("Aberdeen", 5);
+        person = new Person();
+    }
+
+    @Test
+    public void hasDestination(){
+        assertEquals("Aberdeen", bus.getDestination());
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(5, bus.getCapacity());
+    }
+
+    @Test
+    public void hasPassengers(){
+        assertEquals(0, bus.passengersCount());
+    }
+
+    @Test
+    public void canAddPassengers(){
+        bus.addPassenger(person);
+        assertEquals(1, bus.passengersCount());
+    }
+
+
+}
