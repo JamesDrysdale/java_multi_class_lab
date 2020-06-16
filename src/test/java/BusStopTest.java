dc007bed6f034391b627dd30a8a3f05f908d391d
Valerian Roustit <valerian.roustit@gmail.com>
2020-06-16 14:36:21 +0100

    add addToQueue

--- src/test/java/BusStopTest.java
+++ src/test/java/BusStopTest.java
@@ -5,14 +5,27 @@ import static org.junit.Assert.assertEquals;
 
 public class BusStopTest {
     private BusStop busStop;
+    private Person person;
 
     @Before
     public void before(){
         busStop = new BusStop("Aberdeen City Center");
+        person = new Person();
     }
 
     @Test
     public void hasName(){
         assertEquals("Aberdeen City Center", busStop.getName());
+    }
+
+    @Test
+    public void hasEmptyQueue(){
+        assertEquals(0, busStop.queueCount());
+    }
+
+    @Test
+    public void canAddPersonToQueue(){
+        busStop.addToQueue(person);
+        assertEquals(1, busStop.queueCount());
     }
 }
